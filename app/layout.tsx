e56cfeb9f2381b44b2eb4cc99f5df73a2545a37c
Shishant Biswas <135415806+shishantbiswas@users.noreply.github.com>
2025-10-11 03:44:40 +0530

    umami config update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
         defer
         id="umami"
         src="https://umami.service.coolify.bsws.in/script.js"
+        data-do-not-track="true"
+        data-domains="bunflix.bsws.in"
         data-website-id="37066bad-b029-48d9-bef5-d3e9abb151cc">
       </Script>
     </html>
